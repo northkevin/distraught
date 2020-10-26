f6db776b30d7450c64b9ff0abb9df7be29774081
Eric Vinson <esvinson@gmail.com>
2020-10-26 15:14:40 -0700

    Add additional parameter options

--- src/lib/statsd.js
+++ src/lib/statsd.js
@@ -2,7 +2,28 @@
 const StatsD = require("hot-shots");
 
 type OptionsType = {
-  prefix: string,
+  host?: string,
+  port?: number,
+  prefix?: string,
+  suffix?: string,
+  tagPrefix?: string,
+  tagSeparator?: string,
+  globalize?: boolean,
+  cacheDns?: boolean,
+  cacheDnsTtl?: number,
+  mock?: boolean,
+  globalTags?: Object,
+  maxBufferSize?: number,
+  bufferFlushInterval?: number,
+  telegraf?: boolean,
+  sampleRate?: number,
+  errorHandler?: function,
+  useDefaultRoute?: boolean,
+  protocol?: string,
+  path?: string,
+  stream?: Object,
+  udsGracefulErrorHandling?: boolean,
+  udsGracefulRestartRateLimit?: number,
 };
 
 module.exports = {
